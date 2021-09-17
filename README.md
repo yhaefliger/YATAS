70b7be3e6b01b13b435c0708406d9f34c809a48c
Yann Haefliger <yhaefliger@gmail.com>
2021-09-17 10:42:14 +0200

    assets versionning update

--- README.md
+++ README.md
@@ -34,3 +34,26 @@ Minified production build
 ```
 npm run build 
 ```
+
+## Assets versioning
+
+This starter includes a small js script to which can add a hash to your assets when building for production builds.
+The script will look for files to include a hash (based on the md5 of the built file content) eg: ```styles.734a7607648afdb.css``` instead of ```styles.css```.
+
+The entry point for matching regular file path to versionned path is generated as an 11ty global data object in ```_data/hash.json``` with the simple structure 
+```json
+{
+  'path/to/resource.css': 'path/to/resource.hash.css'
+}
+```
+
+In your 11ty template, you can then simply retrieve the versioned path from the array ```{{ hash['path/to/resource.css'] }}``` 
+
+If you want to add other assets to this generated data array, simply include the path in the ```assets``` const in the ```hash.js``` file in the root directory (files listed below are already included).
+
+```js
+const assets = [
+  'css/styles.css',
+  'js/scripts.js'
+];
+```
--- hash.js
+++ hash.js
@@ -8,20 +8,26 @@ const assets = [
 
 const dataFile = '_data/hash.json';
 
+const production = process.env.NODE_ENV === 'development' ? false:true;
+
 let jsonValue = {};
 
 assets.forEach((asset) => {
-  let file = '_site/' + asset;
-  let fileHash = md5(fs.readFileSync(file)).substring(0, 15);
-
-  let assetNameArray = asset.split('.');
-  assetNameArray.splice(assetNameArray.length - 1, 0, fileHash);
-  let hashedAsset = assetNameArray.join('.');
-
+  if (production) {
+    let file = '_site/' + asset;
+    let fileHash = md5(fs.readFileSync(file)).substring(0, 15);
   
-  fs.renameSync(file, '_site/' + hashedAsset);
+    let assetNameArray = asset.split('.');
+    assetNameArray.splice(assetNameArray.length - 1, 0, fileHash);
+    let hashedAsset = assetNameArray.join('.');
+  
+    
+    fs.renameSync(file, '_site/' + hashedAsset);
+    jsonValue[asset] = hashedAsset;
+  } else {
+    jsonValue[asset] = asset;
+  }
 
-  jsonValue[asset] = hashedAsset;
 });
 
 fs.writeFileSync(dataFile, JSON.stringify(jsonValue));
