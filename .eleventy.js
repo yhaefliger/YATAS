607250019801ec9c9762860e310727072579eb46
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2022-04-22 15:03:42 +0200

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
-const htmlmin = require('html-minifier')
+const htmlmin = require("html-minifier");
 const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
+const sharpPlugin = require("eleventy-plugin-sharp");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   /**
    * Upgrade helper
    * Uncomment if you need help upgrading to new major version.
@@ -9,35 +10,45 @@ module.exports = function(eleventyConfig) {
   //eleventyConfig.addPlugin(UpgradeHelper);
 
   /**
+   * https://github.com/luwes/eleventy-plugin-sharp
+   */
+  eleventyConfig.addPlugin(
+    sharpPlugin({
+      urlPath: "/img",
+      outputDir: "./_site/img",
+    })
+  );
+
+  /**
    * Files to copy
    * https://www.11ty.dev/docs/copy/
    */
-  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy("src/img");
 
   /**
    * HTML Minifier for production builds
    */
-  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (
-      process.env.ELEVENTY_ENV == 'production' &&
+      process.env.ELEVENTY_ENV == "production" &&
       outputPath &&
-      outputPath.endsWith('.html')
+      outputPath.endsWith(".html")
     ) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
-      })
-      return minified
+      });
+      return minified;
     }
 
-    return content
-  })
+    return content;
+  });
 
   return {
     dir: {
       input: "src",
-      data: "../_data"
-    }
+      data: "../_data",
+    },
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ['./src/**/*.njk', './src/**/*.md',],
+  content: ["./src/**/*.njk", "./src/**/*.md"],
   theme: {
-    extend: {},
+    fontSize: {
+      ...defaultTheme.fontSize,
+      "10xl": "9rem",
+      "11xl": "10rem",
+      "12xl": "11rem",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.stone,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Merriweather", ...defaultTheme.fontFamily.serif],
+        heading: ["Anton", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
