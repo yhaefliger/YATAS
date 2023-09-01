9174a7c1197565697601b6c47c78b97d2bc766ce
Aleksandr Zapparov <alexander@zapparov.co.uk>
2023-09-01 19:32:30 +0200

    clean up code

--- .eleventy.js
+++ .eleventy.js
@@ -1,69 +1,72 @@
 const htmlmin = require('html-minifier')
 const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
-const { DateTime } = require("luxon");
+const {DateTime} = require("luxon");
 const fs = require('fs')
 
-module.exports = function(eleventyConfig) {
-  /**
-   * Upgrade helper
-   * Uncomment if you need help upgrading to new major version.
-   */
-  //eleventyConfig.addPlugin(UpgradeHelper);
+module.exports = function (eleventyConfig) {
+    /**
+     * Upgrade helper
+     * Uncomment if you need help upgrading to new major version.
+     */
+    //eleventyConfig.addPlugin(UpgradeHelper);
 
-  /**
-   * Files to copy
-   * https://www.11ty.dev/docs/copy/
-   */
+    /**
+     * Files to copy
+     * https://www.11ty.dev/docs/copy/
+     */
 
-  const getSvgContent = function (fileName, classes = '') {
-    const relativeFilePath = `./src/svg/${fileName}.svg`;
-    let data = fs.readFileSync(relativeFilePath,
-        function(err, contents) {
-          if (err) return err
-          return contents
-        });
+    const getSvgContent = function (fileName, classes = '') {
+        const relativeFilePath = `./src/svg/${fileName}.svg`;
+        let data = fs.readFileSync(relativeFilePath,
+            function (err, contents) {
+                if (err) return err
+                return contents
+            });
 
-    data = data.toString('utf8');
+        data = data.toString('utf8');
 
-    if (classes) {
-      data = data.replace("<svg", `<svg class="${classes}"`)
+        if (classes) {
+            data = data.replace("<svg", `<svg class="${classes}"`)
+        }
+
+        return data
     }
 
-    return data
-  }
+    eleventyConfig.addShortcode("svg", getSvgContent);
 
-  eleventyConfig.addShortcode("svg", getSvgContent);
+    eleventyConfig.addPassthroughCopy('src/img')
 
-  eleventyConfig.addPassthroughCopy('src/img')
+    /**
+     * HTML Minifier for production builds
+     */
+    eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+        if (
+            process.env.ELEVENTY_ENV == 'production' &&
+            outputPath &&
+            outputPath.endsWith('.html')
+        ) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true,
+            })
+            return minified
+        }
 
-  /**
-   * HTML Minifier for production builds
-   */
-  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
-    if (
-      process.env.ELEVENTY_ENV == 'production' &&
-      outputPath &&
-      outputPath.endsWith('.html')
-    ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-      })
-      return minified
-    }
+        return content
+    })
 
-    return content
-  })
+    eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+        const date = DateTime.fromJSDate(new Date(dateObj), {zone: zone || "utc"}).toFormat(format || "dd LLLL, yyyy");
+        // console.log(typeof date, date);
+        return date;
+    });
 
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromFormat(dateObj, "yyyy-dd-mm").toLocaleString(DateTime.DATE_MED);
-  });
-
-  return {
-    dir: {
-      input: "src",
-      data: "../_data"
-    }
-  };
+    return {
+        dir: {
+            input: "src",
+            data: "../_data"
+        }
+    };
 };
+
